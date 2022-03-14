36a77b41b3a83775ebad625d95cc0a044042ed3a
nastya <nastya.vergeeva@mail.ru>
2022-03-14 10:47:02 +0500

    По кнопке можно  добавить данные в таблицу

--- Form1.cs
+++ Form1.cs
@@ -4,11 +4,13 @@ using System.Windows.Forms;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Net.Http;
+using System.Data.SqlClient;
 
 namespace Валюты
 {
     public partial class Form1 : Form
     {
+        List<Stock> stocks = new List<Stock>();
         private const string RequestUri = "https://quote.rbc.ru/v5/ajax/catalog/get-tickers?type=share&sort=blue_chips&limit=200&offset=0";
 
         public class Stock
@@ -35,6 +37,7 @@ namespace Валюты
         {
             InitializeComponent();
             dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
         }
 
         private async void button1_Click(object sender, EventArgs e)
@@ -50,7 +53,7 @@ namespace Валюты
 
 
 
-            var stocks = JsonSerializer.Deserialize<List<Stock>>(jsonString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            stocks = JsonSerializer.Deserialize<List<Stock>>(jsonString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
             if (stocks != null)
             {
                 foreach (var element in stocks!)
@@ -64,7 +67,26 @@ namespace Валюты
         private void button2_Click(object sender, EventArgs e)
         {
             //Скачать в бд
-            
+            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\nastya\Desktop\6сем\Сартасов_ВССИТ\Валюты\Database1.mdf;Integrated Security=True";
+            System.Data.SqlClient.SqlConnection sqlConnection1 = new System.Data.SqlClient.SqlConnection(connectionString);
+
+            SqlCommand cmnd = new SqlCommand("DELETE FROM [Table]", sqlConnection1);
+            cmnd.Connection.Open();
+            cmnd.ExecuteNonQuery();
+            sqlConnection1.Close();
+
+            foreach (var element in stocks)
+            {
+                SqlCommand command = new SqlCommand("INSERT INTO [Table] (NameOfCompany, StockPrice, Currency) VALUES (@value1,@value2,@value3)", sqlConnection1);
+                command.Parameters.AddWithValue("@value1", element.Company.Title);
+                command.Parameters.AddWithValue("@value2", element.Price);
+                command.Parameters.AddWithValue("@value3", element.Currency);
+
+                command.Connection.Open();
+                command.ExecuteNonQuery();
+                sqlConnection1.Close();
+            }
+            MessageBox.Show("Сохранение в базу данных выполнено!");
         }
     }
 }
